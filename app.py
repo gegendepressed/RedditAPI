635ae321d577de482acbad9bd86f537c6c3dec8e
gegendepressed <sairaj09pai@gmail.com>
2024-09-05 22:18:52 +0530

    app.py : added top and hot endpoints

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import praw
+import random
 from dotenv import load_dotenv
 import os
+from datetime import datetime
+import praw.exceptions
 
 app = FastAPI()
 app.add_middleware(
@@ -25,15 +28,94 @@ reddit = praw.Reddit(
 )
 
 @app.get("/{subreddit}" , status_code=201)
-def fetch(subreddit: str):
+def fetchrandom(subreddit: str):
     try:
         submission = reddit.subreddit(subreddit).random()
+        submission_date = datetime.utcfromtimestamp(submission.created_utc).strftime('%d-%m-%y')        
+
         return { 
                 "title": submission.title,
                 "description": submission.selftext,
                 "url" : submission.url,
+                "date" : submission_date,
         }
+    except praw.exceptions.APIException as e:
+        raise HTTPException(status_code=500, detail=f"Reddit API error!")
     except Exception as e:
-        raise HTTPException(status_code=404, detail="Subreddit not found")
+        raise HTTPException(status_code=500, detail=f"An unexpected error occurred !")
+
+@app.get("/{subreddit}/top/pastweek" , status_code=200)
+def fetchtop_week(subreddit):
+   try:
+        submissions = list(reddit.subreddit(subreddit).top('week', limit=100))
+        submission = random.choice(submissions)
+        submission_date = datetime.utcfromtimestamp(submission.created_utc).strftime('%d-%m-%y')        
+        return {
+            "title": submission.title,
+            "description": submission.selftext,
+            "url": submission.url,
+            "date" : submission_date,
+        }
+   except praw.exceptions.APIException as e:
+        raise HTTPException(status_code=500, detail=f"Reddit API error!")
+   except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An unexpected error occurred !")
+
+   
+@app.get("/{subreddit}/top/pastmonth" , status_code=200)
+def fetchtop_month(subreddit):
+   try:
+        submissions = list(reddit.subreddit(subreddit).top('month', limit=100))
+        submission = random.choice(submissions)
+        submission_date = datetime.utcfromtimestamp(submission.created_utc).strftime('%d-%m-%y')        
+        return {
+            "title": submission.title,
+            "description": submission.selftext,
+            "url": submission.url,
+            "date" : submission_date,
+        }
+   except praw.exceptions.APIException as e:
+        raise HTTPException(status_code=500, detail=f"Reddit API error!")
+   except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An unexpected error occurred !")
+
+   
+@app.get("/{subreddit}/top/pastyear" , status_code=200)
+def fetchtop_year(subreddit):
+   try:
+        submissions = list(reddit.subreddit(subreddit).top('year', limit=100))
+        submission = random.choice(submissions)
+        submission_date = datetime.utcfromtimestamp(submission.created_utc).strftime('%d-%m-%y')        
+        return {
+            "title": submission.title,
+            "description": submission.selftext,
+            "url": submission.url,
+            "date" : submission_date,
+        }
+   except praw.exceptions.APIException as e:
+        raise HTTPException(status_code=500, detail=f"Reddit API error!")
+   except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An unexpected error occurred !")
+
+@app.get("/{subreddit}/hot" , status_code=200)
+def fetchtop_month(subreddit):
+   try:
+        submissions = list(reddit.subreddit(subreddit).hot(limit=100))
+        submission = random.choice(submissions)
+        submission_date = datetime.utcfromtimestamp(submission.created_utc).strftime('%d-%m-%y')        
+        return {
+            "title": submission.title,
+            "description": submission.selftext,
+            "url": submission.url,
+            "date" : submission_date,
+        }
+   except praw.exceptions.APIException as e:
+        raise HTTPException(status_code=500, detail=f"Reddit API error!")
+   except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An unexpected error occurred !")
+
+
+
+    
 
     
